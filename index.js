5734987cc812813f9affb45b07d56b1d35e7d614
dphong08 <dphongg199x@gmail.com>
2021-03-04 16:18:24 +0700

    up

---
+++ index.js
@@ -0,0 +1,10 @@
+let inputWidth
+let inputHeight
+inputWidth = prompt("Enter the width")
+inputHeight = prompt("Enter the height")
+let width = inputWidth
+let height = inputHeight
+let area = width * height
+document.writeln("The width is : " + width);
+document.writeln("The height is : " + height);
+document.write("The area is : " + area)
